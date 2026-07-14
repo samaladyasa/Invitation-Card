cd095fa92c7fd42dd34a58936cfc723511557908
samaladyasa <adyasasamal2004@gmail.com>
2026-07-14 12:03:01 +0530

    Update ScrollReveal.jsx

--- src/components/ScrollReveal.jsx
+++ src/components/ScrollReveal.jsx
@@ -10,12 +10,10 @@ export default function ScrollReveal({
       initial={{
         opacity: 0,
         y: 50,
-        filter: "blur(8px)",
       }}
       whileInView={{
         opacity: 1,
         y: 0,
-        filter: "blur(0px)",
       }}
       viewport={{
         once: true,
