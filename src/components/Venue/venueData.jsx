83ce7f4c31335e8b16d2605e32e902c777b90569
samaladyasa <adyasasamal2004@gmail.com>
2026-07-17 20:33:39 +0530

    Update venueData.jsx

--- src/components/Venue/venueData.jsx
+++ src/components/Venue/venueData.jsx
@@ -1,12 +1,10 @@
+import details from "../../data/details.json";
+
 const venueData = {
-  name: "Mayfair Lagoon",
-  location: "Bhubaneswar, Odisha",
-
-  address:
-    "Jaydev Vihar, Bhubaneswar, Odisha 751013",
-
-  mapLink:
-    "https://maps.app.goo.gl/qWrc5NyAFRxavbom8",
+  name: details.venue,
+  location: details.venueLocation,
+  address: details.venueAddress,
+  mapLink: details.venueMapLink,
 };
 
 export default venueData;
